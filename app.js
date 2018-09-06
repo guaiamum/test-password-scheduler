01c7e67aefc3340d6d1f3eb6752ff16140c074d4
Cainã Brazil <caina.brazil@jumia.com>
2018-09-06 10:17:46 +0100

    opens page...

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const URL = require('./info.js')
+const info = require('./info.js')
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
-  await page.goto(URL)
-  await page.focus('oldPassword')
-  await page.keyboard.type('keyboard.png' )
-  
-  const content = await page.content();
-  console.log(content);
+  await page.setViewport({ width: 1280, height: 800 })
+  await page.goto(info.URL)
 
-  await browser.close()
+  //populates fields
+  await page.focus(info.USER_FIELD)
+  await page.keyboard.type(info.USER)
+  await page.focus(info.PASS_FIELD)
+  await page.keyboard.type(info.PASS)
+  
+  // await page.$eval('form', form => form.submit());
+  // const text = page.evaluate(() => document.querySelector('div.msg_yes').textContent);
+  // console.log(text);
+
+  // await browser.close()
 })()
