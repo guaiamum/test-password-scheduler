63e8caa085fc1f4b8c73ff6e40424ef67e01769f
Cainã Brazil <caina.brazil@jumia.com>
2018-09-10 18:15:51 +0100

    Sending message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const DEBUG = false;
 const info = require('./info.js');
 const puppeteer = require('puppeteer');
+const { exec } = require('child_process');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: !DEBUG });
@@ -10,17 +11,17 @@ const puppeteer = require('puppeteer');
   await page.goto(info.URL);
 
   //populates fields
-  await page.focus(info.USER_FIELD)
-  await page.keyboard.type(info.USER)
-  await page.focus(info.PASS_FIELD)
-  await page.keyboard.type(info.PASS)
+  await page.focus(info.USER_FIELD);
+  await page.keyboard.type(info.USER);
+  await page.focus(info.PASS_FIELD);
+  await page.keyboard.type(info.PASS);
 
   //submitting form
   await Promise.all([page.evaluate((info) => {
       return document.querySelector(info.SUBMIT_BUTTON).click();
     }, info),
     page.waitForNavigation()
-  ])
+  ]);
 
   //fetching  result
   const success = await page.$eval(info.SUCCESS_SELECTOR, (el) => el.outerHTML || null).catch((err) => {
@@ -29,7 +30,15 @@ const puppeteer = require('puppeteer');
   });
 
   if (success) {
-    console.log('\n\n', getDate(success));
+    let message = 'Hi';
+    let shell = '';
+
+    if(process.platform === 'linux') {
+      shell = 'notify-send';
+      message = getParagraphsContent(success)[1];
+    }
+    
+    exec(`${shell} \'${message}\'`)
   }
 
   if (!DEBUG)
@@ -42,11 +51,11 @@ function getParagraphs(str) {
   return str.match(/<p>.*?<\/p>/g);
 }
 
-function getParagraphContent(str) {
+function getParagraphsContent(str) {
   return getParagraphs(str).map((e)=>e.replace(/<p>|<\/p>/g,''));
 }
 
 function getDate(str) {
-  const contentArray = getParagraphContent(str);
+  const contentArray = getParagraphsContent(str);
   return contentArray[1].substr(contentArray.length-12);
 }
