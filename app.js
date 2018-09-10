1a1a5c1d0848cc94b8336ea8dd147089f729c3c5
Cainã Brazil <caina.brazil@jumia.com>
2018-09-10 17:08:12 +0100

    comunicating and getting response

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-const info = require('./info.js')
+const DEBUG = false;
+const info = require('./info.js');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: !DEBUG });
   const page = await browser.newPage();
 
-  await page.setViewport({ width: 1280, height: 800 })
-  await page.goto(info.URL)
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto(info.URL);
 
   //populates fields
   await page.focus(info.USER_FIELD)
   await page.keyboard.type(info.USER)
   await page.focus(info.PASS_FIELD)
   await page.keyboard.type(info.PASS)
-  
-  // await page.$eval('form', form => form.submit());
-  // const text = page.evaluate(() => document.querySelector('div.msg_yes').textContent);
-  // console.log(text);
 
-  // await browser.close()
+  //submitting form
+  await Promise.all([page.evaluate((info) => {
+      return document.querySelector(info.SUBMIT_BUTTON).click();
+    }, info),
+    page.waitForNavigation()
+  ])
+
+  //fetching  result
+  const success = await page.$eval(info.SUCCESS_SELECTOR, (el) => el.outerHTML || null).catch((err) => {
+    console.log('\nErro ao fazer Login! Reveja suas credenciais!');
+    return false;
+  });
+
+  if (success) {
+    console.log('\n\n', success);
+  }
+
+  if (!DEBUG)
+    await browser.close();
+
+  return;
 })()
--- info.js
+++ info.js
@@ -1,1 +1,9 @@
-module.exports = URL = 'https://adpasswordchange.ptc.lan/index_test.php';
+module.exports = {
+    URL: 'https://my-url.com',
+    USER: 'username',
+    USER_FIELD: '[name="username"]',
+    PASS: 'my-pass',
+    PASS_FIELD: '[name="password"]',
+    SUBMIT_BUTTON: '[name="submit"]',
+    SUCCESS_SELECTOR: '.success'
+};
